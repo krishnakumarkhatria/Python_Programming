735dbc2abda87b4cdf4eb4851d94b98b49319224
krishnakumarkhatria <krishkhatria@gmail.com>
2026-01-17 19:24:32 +0530

    operators in python

---
+++ operators.py
@@ -0,0 +1,72 @@
+#print(5+2*3-1+10/5)
+
+
+
+
+# BMI:
+# weight = input("your weight:")
+# height = input("your height:")
+
+# # print(type(height))
+# total = int(weight)/float(height)**2
+# print(int(total))
+
+
+# a,b=4,3
+# c=a+b
+# a +=2
+# c//=a
+# print(c)
+
+
+
+# a=5
+# print(a==5)
+# print(a<6)
+
+
+
+
+
+
+#logical operators:
+# in "and" condition it will not check the second condition
+# but in "or" condition it will check the second condition
+# a,b=4,5
+# print(a<3 and b==5)
+# print(a<3 or b==5)
+
+
+
+
+#bitwise operators:
+# a,b= 4,3
+# print(a&b)
+
+
+
+
+
+
+# identity operator:
+# a=5   # it will be true when a = 5 or b= 4
+# b=4
+# print(a is b)
+# print(id(a))
+# print(id(b))
+# print(a is not b) #will be true if both are equal
+
+
+
+
+
+
+#membership operator:
+# str ='Krishna'
+# print('j'in str)
+# print("k"not in str)
+# print("K"in str)
+
+
+
+
