ee49579bc3000133b2d560d1d2e6b32304edc5d5
krishnakumarkhatria <krishkhatria@gmail.com>
2026-01-17 19:04:49 +0530

    addition of integers

---
+++ add.py
@@ -0,0 +1,16 @@
+#add digits of two numbers:
+#method: 1
+# a = input("enter the first number:")
+# b = input("enter the second number:")
+# sum= int(a) +int(b)
+
+# print(sum)
+
+
+
+#method:2
+num = input("enter two digit number: ")
+a = num[0]
+b = num[1]
+
+print(int(a)+int(b))
